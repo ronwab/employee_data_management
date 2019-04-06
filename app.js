b4f543f6b300e212f56ddde308dd8dcbba31a313
n00b-c0der <twh710twh@gmail.comm>
2019-04-06 10:35:09 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,16 @@
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyCLrtq5JwyULD4R6Dc_Mb_YbryzceS9mJs",
+    authDomain: "gtcbc-coding.firebaseapp.com",
+    databaseURL: "https://gtcbc-coding.firebaseio.com",
+    projectId: "gtcbc-coding",
+    storageBucket: "gtcbc-coding.appspot.com",
+    messagingSenderId: "46793546586"
+  };
+  firebase.initializeApp(config);
+
+
+// Assign the reference to the database to a variable named 'database'
+// var database = ...
+var db = firebase.database();
+
